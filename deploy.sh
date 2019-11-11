42e64599927a4ef887b66e2a0805bd713f007720
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2019-11-11 08:54:09 -0600

    added reference.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
-# Deploy kubeless with non-RBAC on Mac.
+# Deploy kubeless with non-RBAC on Mac [1].
 export RELEASE=$(curl -s https://api.github.com/repos/kubeless/kubeless/releases/latest | grep tag_name | cut -d '"' -f 4)
 kubectl create ns kubeless
 kubectl create -f https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless-non-rbac-$RELEASE.yaml
 kubectl get pods -n kubeless
 kubectl get deployment -n kubeless
 kubectl get customresourcedefinition
+
+# References
+# [1] https://kubeless.io/docs/quick-start/
